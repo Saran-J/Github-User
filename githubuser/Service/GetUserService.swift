e9b1b6a8116e4e74299900f1b478944eefe006ef
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-18 01:58:59 +0700

    add Github UserItem Model in GetUserService

--- githubuser/Service/GetUserService.swift
+++ githubuser/Service/GetUserService.swift
@@ -3,11 +3,49 @@ import Moya
 import RxSwift
 
 struct UserItem: Codable {
-    var id: Int64
+    var login: String?
+    var id: Int?
+    var nodeId: String?
+    var avatarUrl: String?
+    var gravatarId: String?
+    var url: String?
+    var htmlUrl: String?
+    var followersUrl: String?
+    var followingUrl: String?
+    var gistsUrl: String?
+    var starredUrl: String?
+    var subscriptionsUrl: String?
+    var organizationsUrl: String?
+    var reposUrl: String?
+    var eventsUrl: String?
+    var receivedEventsUrl: String?
+    var type: String?
+    var siteAdmin: Bool?
+    
+    enum CodingKeys: String, CodingKey {
+        case login = "login"
+        case id = "id"
+        case nodeId = "node_id"
+        case avatarUrl = "avatar_url"
+        case gravatarId = "gravatar_id"
+        case url = "url"
+        case htmlUrl = "html_url"
+        case followersUrl = "followers_url"
+        case followingUrl = "following_url"
+        case gistsUrl = "gists_url"
+        case starredUrl = "starred_url"
+        case subscriptionsUrl = "subscriptions_url"
+        case organizationsUrl = "organizations_url"
+        case reposUrl = "repos_url"
+        case eventsUrl = "events_url"
+        case receivedEventsUrl = "received_events_url"
+        case type = "type"
+        case siteAdmin = "site_admin"
+    }
 }
 
-class GetUserService: BaseService<UserProvider, UserItem> {
-    func executeService(page: Int, perPage: Int) -> Observable<UserItem> {
+class GetUserService: BaseService<UserProvider, [UserItem]> {
+    func executeService(page: Int, perPage: Int) -> Observable<[UserItem]> {
         return super.callService(
             target: UserProvider.fetchUser(page: page, perPage: perPage))
     }
