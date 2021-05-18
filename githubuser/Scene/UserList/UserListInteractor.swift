0f8daaad91825420306f7f1dde19462044b3992c
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-19 00:11:54 +0700

    define function fetchUserList and searchUser in UserListInteractor

--- githubuser/Scene/UserList/UserListInteractor.swift
+++ githubuser/Scene/UserList/UserListInteractor.swift
@@ -1,11 +1,39 @@
 import UIKit
+import RxSwift
 
 protocol UserListBusinessLogic {
+    func fetchUserList(request: UserList.FetchUserList.Request)
+    func searchUser(request: UserList.SearchUser.Request)
 }
 
 protocol UserListDataStore {
 }
 
 class UserListInteractor: UserListBusinessLogic, UserListDataStore {
+    var perPage: Int = 10
+    var pageNo: Int = 0
+    var disposeBag = DisposeBag()
+    
     var presenter: UserListPresentationLogic?
+    var userListService = GetUserService()
+    var searchUserService = SearchUserService()
+    func fetchUserList(request: UserList.FetchUserList.Request) {
+        userListService.executeService(page: pageNo, perPage: perPage)
+            .subscribe { userList in
+                print(userList)
+            } onError: { error in
+                print(error)
+            }
+        .disposed(by: disposeBag)
+    }
+    
+    func searchUser(request: UserList.SearchUser.Request) {
+        searchUserService.executeService(keyword: request.keyword)
+            .subscribe { searchResponse in
+                print(searchResponse)
+            } onError: { error in
+                print(error)
+            }
+        .disposed(by: disposeBag)
+    }
 }
