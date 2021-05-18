dfd075bc52eff9efefdff03b6da23b4a85d2fb56
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-19 00:45:17 +0700

    implement UserList Presentation Logic

--- githubuser/Scene/UserList/UserListPresenter.swift
+++ githubuser/Scene/UserList/UserListPresenter.swift
@@ -9,8 +9,30 @@ class UserListPresenter: UserListPresentationLogic {
     weak var viewController: UserListDisplayLogic?
     
     func presentUserList(response: UserList.FetchUserList.Response) {
+        var userList: [UserListObject] = []
+        response.userListRespnse.forEach { user in
+            let userObject = UserListObject(
+                name: toString(user.login),
+                url: toString(user.url),
+                avatarImageUrl: toString(user.avatarUrl))
+            userList.append(userObject)
+        }
+        let viewModel = UserList.FetchUserList.ViewModel(
+            userListDisplay: userList)
+        viewController?.displayUserList(viewModel: viewModel)
     }
     
     func presentSearchUser(response: UserList.SearchUser.Response) {
+        var userList: [UserListObject] = []
+        response.searchResponse.items.forEach { user in
+            let userObject = UserListObject(
+                name: toString(user.login),
+                url: toString(user.url),
+                avatarImageUrl: toString(user.avatarUrl))
+            userList.append(userObject)
+        }
+        let viewModel = UserList.SearchUser.ViewModel(
+            userListDisplay: userList)
+        viewController?.displaySearchUser(viewModel: viewModel)
     }
 }
