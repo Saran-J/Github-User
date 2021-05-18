023ebb1bfe9fd8019b5f9db7818f8bfad6cdd979
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-18 23:59:00 +0700

    define FetchUserList case

--- githubuser/Scene/UserList/UserListModels.swift
+++ githubuser/Scene/UserList/UserListModels.swift
@@ -1,5 +1,19 @@
 import UIKit
 
 enum UserList {
-    enum Something {}
+    enum FetchUserList {
+        struct Request {}
+        struct Response {
+            var userListRespnse: [UserItem]
+        }
+        struct ViewModel {
+            var userListDisplay: [UserListObject]
+        }
+    }
+}
+
+struct UserListObject {
+    var name: String
+    var url: String
+    var avatarImageUrl: String
 }
