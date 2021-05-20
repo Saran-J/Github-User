dd85ce8ead6c9b6ff624859bea9b04434ee84113
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-20 23:00:06 +0700

    remove unuse worker

--- githubuser/Scene/UserRepos/UserReposInteractor.swift
+++ githubuser/Scene/UserRepos/UserReposInteractor.swift
@@ -9,8 +9,6 @@ protocol UserReposDataStore {
 
 class UserReposInteractor: UserReposBusinessLogic, UserReposDataStore {
     var presenter: UserReposPresentationLogic?
-    var worker: UserReposWorker?
-    
     func fetchUserRepository(request: UserRepos.FetchUserRepository.Request) {
     }
 }
