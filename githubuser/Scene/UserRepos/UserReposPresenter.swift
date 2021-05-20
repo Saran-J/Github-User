8d0fa119c326f67055b429d75e79bdec2be41ac3
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-20 23:22:52 +0700

    implement presentation logic for FetchUserRepos

--- githubuser/Scene/UserRepos/UserReposPresenter.swift
+++ githubuser/Scene/UserRepos/UserReposPresenter.swift
@@ -8,5 +8,21 @@ class UserReposPresenter: UserReposPresentationLogic {
     weak var viewController: UserReposDisplayLogic?
     
     func presentUserRepository(response: UserRepos.FetchUserRepository.Response) {
+        let repositoryList: [RepositoryObject] =
+            response.userRepository.map { repo -> RepositoryObject in
+            let object = RepositoryObject(
+                title: toString(repo.name),
+                detail: toString(repo.description),
+                language: toString(repo.language))
+                return object
+            }
+        let detail = RepositoryDetail(
+            name: toString(response.userDetail.login),
+            url: toString(response.userDetail.url),
+            avatarImageUrl: toString(response.userDetail.avatarUrl),
+            isFavorite: toBool(response.userDetail.favorite),
+            repository: repositoryList)
+        let viewModel = UserRepos.FetchUserRepository.ViewModel(repositoryObject: detail)
+        viewController?.displayUserRepository(viewModel: viewModel)
     }
 }
