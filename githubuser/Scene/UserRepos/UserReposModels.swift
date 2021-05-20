70e34367ad50b545bdb3964eec271f7e9a6df71c
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-20 22:54:36 +0700

    define Model for UserRepos

--- githubuser/Scene/UserRepos/UserReposModels.swift
+++ githubuser/Scene/UserRepos/UserReposModels.swift
@@ -1,8 +1,30 @@
 import UIKit
 enum UserRepos {
-    enum Something {
-        struct Request {}
-        struct Response {}
-        struct ViewModel {}
+    enum FetchUserRepository {
+        struct Request {
+            var user: String
+        }
+        struct Response {
+            var userRepository: [GetUserRepoResponse]
+            var userDetail: UserItem
+            var isLastPage: Bool
+        }
+        struct ViewModel {
+            var repositoryObject: RepositoryDetail
+        }
     }
+}
+
+struct RepositoryDetail {
+    var name: String
+    var url: String
+    var avatarImageUrl: String
+    var isFavorite: Bool
+    var repository: [RepositoryObject]
+}
+
+struct RepositoryObject {
+    var title: String
+    var detail: String
+    var language: String
 }
