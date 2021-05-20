816a612eaff715ea02ae7c520609f3eb64790d10
Saran Jantara-amornporn <saran.j2108@gmail.com>
2021-05-20 23:38:44 +0700

    setup repoHeaderView setup repo tableview

--- githubuser/Scene/UserRepos/UserReposViewController.swift
+++ githubuser/Scene/UserRepos/UserReposViewController.swift
@@ -7,6 +7,10 @@ protocol UserReposDisplayLogic: class {
 class UserReposViewController: UIViewController {
     var interactor: UserReposBusinessLogic?
     var router: (NSObjectProtocol & UserReposRoutingLogic & UserReposDataPassing)?
+    var userRepositoryData: RepositoryDetail?
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var headerView: RepoHeaderView!
     
     // MARK: Object lifecycle
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -51,5 +55,39 @@ class UserReposViewController: UIViewController {
 
 extension UserReposViewController: UserReposDisplayLogic {
     func displayUserRepository(viewModel: UserRepos.FetchUserRepository.ViewModel) {
+        userRepositoryData = viewModel.repositoryObject
+        headerView.setupData(
+            title: toString(userRepositoryData?.name),
+            url: toString(userRepositoryData?.url),
+            image: toString(userRepositoryData?.avatarImageUrl)
+        )
+        tableView.reloadData()
+    }
+}
+
+extension UserReposViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toInt(userRepositoryData?.repository.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: "repoCell") as? RepoCell,
+            let repoObject = userRepositoryData?.repository[indexPath.row]
+        else {
+            return UITableViewCell()
+        }
+        cell.titleLabel.text = repoObject.title
+        cell.detailLabel.text = repoObject.detail
+        cell.languageLabel.text = repoObject.language
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return self.headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return RepoHeaderView.headerHeight
     }
 }
--- githubuser/Scene/UserRepos/tableviewCell/RepoHeaderView.swift
+++ githubuser/Scene/UserRepos/tableviewCell/RepoHeaderView.swift
@@ -1,7 +1,27 @@
 import UIKit
+import Kingfisher
 
 class RepoHeaderView: UIView {
+    static let headerHeight: CGFloat = 120
+    
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var urlLabel: UILabel!
+    
+    func setupData(title: String, url: String, image: String) {
+        titleLabel.text = title
+        urlLabel.text = url
+        
+        let processor = DownsamplingImageProcessor(size: avatarImageView.bounds.size)
+        let url = URL(string: image)
+        avatarImageView.kf.setImage(
+            with: url,
+            placeholder: UIImage(named: "placeholderImage"),
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+    }
 }
